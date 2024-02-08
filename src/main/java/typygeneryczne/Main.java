8a08a6f37bb221bbcab8026181cfe5fdfa775206
maciejkawka1 <kawkamaciej.mk@gmail.com>
2024-02-08 20:57:22 +0100

    poprawa generycznego

--- src/main/java/typygeneryczne/Main.java
+++ src/main/java/typygeneryczne/Main.java
@@ -27,13 +27,6 @@ public class Main {
     }
 
     public static  <T extends Number> void swapOnlyNumbers(T[] array, int position1, int position2){
-        if (position1 < 0 || position1 >= array.length || position2 < 0 || position2 >= array.length) {
-            System.out.println("zla pozycja");
-            return;
-        }
-
-        T temp = array[position1];
-        array[position1] = array[position2];
-        array[position2] = temp;
+        swapArray(array,position1,position2);
     }
 }
