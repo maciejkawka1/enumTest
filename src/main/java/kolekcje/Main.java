ecf650d54d2812ee417b306866b10d8f47283827
maciejkawka1 <kawkamaciej.mk@gmail.com>
2024-02-12 18:33:48 +0100

    kolekcje, zad 1 - konwersja stringow

---
+++ src/main/java/kolekcje/Main.java
@@ -0,0 +1,51 @@
+package kolekcje;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Napisz mechanizm, który skonwertuje listę typu String na listę typu Integer
+
+        List<String> strList = new ArrayList<>();
+        strList.add("123");
+        strList.add("1");
+        strList.add("85");
+        strList.add("444444");
+        strList.add("3");
+        strList.add("2SBBBC");
+        strList.add("567");
+        strList.add("2499");
+        System.out.println("wielkosc sringow: "+strList.size());
+        List<Integer> integers = convertListToIntegers(strList);
+        System.out.println("wielkosc int: " + integers.size());
+        for(Integer i : integers){
+            System.out.println(i);
+        }
+    }
+
+    public static List<Integer> convertListToIntegers(List<String> listOfStrings){
+        List<Integer> listOfIntegers = new ArrayList<>();
+
+        for(String element : listOfStrings){
+            if(isValidString(element)){
+                listOfIntegers.add(Integer.parseInt(element));
+            }
+        }
+
+        return listOfIntegers;
+    }
+
+    private static boolean isValidString(String element) {
+
+        try {
+            Integer.parseInt(element);
+            return true;
+        }catch (NumberFormatException e){
+            return false;
+        }
+    }
+
+
+}
